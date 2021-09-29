94f3a2d5718b4ca496b0960eb1166f5046030403
shahaas <shahaask111@gmail.com>
2021-09-29 08:30:01 +0530

    added fill project

---
+++ build_nrf9160dk_nrf9160ns/zephyr/nrf9160dk_nrf9160ns.dts.pre.d
@@ -0,0 +1,15 @@
+empty_file.o: C:/Users/Shahajas/ncs/v1.5.1/zephyr/misc/empty_file.c \
+ C:/Users/Shahajas/ncs/v1.5.1/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160ns.dts \
+ C:/Users/Shahajas/ncs/v1.5.1/zephyr/dts/arm/nordic/nrf9160ns_sica.dtsi \
+ C:/Users/Shahajas/ncs/v1.5.1/zephyr/dts/common/mem.h \
+ C:/Users/Shahajas/ncs/v1.5.1/zephyr/dts/arm/nordic/nrf9160ns.dtsi \
+ C:/Users/Shahajas/ncs/v1.5.1/zephyr/dts/arm/armv8-m.dtsi \
+ C:/Users/Shahajas/ncs/v1.5.1/zephyr/dts/common/skeleton.dtsi \
+ C:/Users/Shahajas/ncs/v1.5.1/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ C:/Users/Shahajas/ncs/v1.5.1/zephyr/include/dt-bindings/i2c/i2c.h \
+ C:/Users/Shahajas/ncs/v1.5.1/zephyr/include/dt-bindings/gpio/gpio.h \
+ C:/Users/Shahajas/ncs/v1.5.1/zephyr/dts/arm/nordic/override.dtsi \
+ C:/Users/Shahajas/ncs/v1.5.1/zephyr/dts/arm/nordic/nrf9160_common.dtsi \
+ C:/Users/Shahajas/ncs/v1.5.1/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160_common.dts \
+ C:/Users/Shahajas/ncs/v1.5.1/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160_partition_conf.dts \
+ C:/AIZON_Projects/ADC/adc/nrf9160dk_nrf9160ns.overlay
